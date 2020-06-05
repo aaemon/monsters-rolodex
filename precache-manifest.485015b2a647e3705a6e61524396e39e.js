bba1c714271b51da8065f7834e0fdd8cdc9fce0f
Ammer Ashraf Emon <aaemon@ip-172-31-69-247.ec2.internal>
2020-06-05 06:03:59 +0000

    Updates

---
+++ precache-manifest.485015b2a647e3705a6e61524396e39e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabb713f9db03707d50facb62fd46fb2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7d818c443246d3257bbf",
+    "url": "/monsters-rolodex/static/css/main.c1ccec19.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d818c443246d3257bbf",
+    "url": "/monsters-rolodex/static/js/main.5e39132d.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
